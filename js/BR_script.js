02e859886e9987a5c3bb39ad93e3d3eb43f8fb16
Ievgen Averianov <averyanovsumy@gmail.com>
2018-06-13 18:15:24 +0300

    slider animation - completed

--- js/BR_script.js
+++ js/BR_script.js
@@ -2,35 +2,20 @@ $(function () {
 
         var $leftUIEl = $('.carousel-arrow-left');
         var $rightUIEl = $('.carousel-arrow-right');
-        // var $elementsList = $('.carousel-list');
-        //
-        // var pixelsOffset = 153;
-        // var currentLeftValue = 0;
-        // var elementsCount = $elementsList.find('li').length;
-        // var minimumOffset = -((elementsCount - 4)*pixelsOffset);
-        // var maximumOffset = 0;
-        //
-        // function checkLeftValue() {
-        //     if (currentLeftValue === minimumOffset) {
-        //         $rightUIEl.addClass('inactive-arrow');
-        //     }
-        //
-        //
-        //     if (currentLeftValue === maximumOffset) {
-        //         $leftUIEl.addClass('inactive-arrow');
-        //     }
-        // }
-        //
         $leftUIEl.click( function () {
-            generateItemLast();
             moveItemAwayFirst();
-            moveItemInLast();
+            setTimeout(function () {
+                generateItemLast();
+                moveItemInLast();
+            }, 400);
         });
         //
         $rightUIEl.click( function () {
-            generateItemFirst();
             moveItemAwayLast();
-            moveItemInFirst();
+            setTimeout(function () {
+                generateItemFirst();
+                moveItemInFirst();
+            }, 400);
         });
 
     function generateItemFirst() {
@@ -38,27 +23,26 @@ $(function () {
         var imagesrc = (randomValue <= 0.5) ? 'img/boy_1.png' : 'img/boy_2.png';
         $('<img>', { src: imagesrc}).appendTo('.empty-first');
         $('<div><div></div></div>').appendTo('.empty-first');
-        $('.first-item').removeClass('first-item')
-        $('.empty-first').removeClass('empty-first').addClass('first-item');
+        $('.empty-first').removeClass('empty-first');
         setTimeout(function () {
             $('<li>', {class: 'carousel-element empty-first'}).prependTo('.carousel-list');
-        }, 2000);
+            $('.empty-first').css({marginLeft: -165+"px"}).animate({marginLeft:0},700);
+        }, 720);
 
     }
 
     function moveItemAwayLast() {
-        $('.last-item').addClass("away-animated-last");
+        $('.carousel-element').eq(3).addClass("away-animated-last");
         setTimeout(function () {
             $('.away-animated-last').remove();
-            $('.carousel-element').eq(3).addClass('last-item')
-        }, 2000);
+        }, 1000);
     }
 
     function moveItemInFirst() {
-        $('.first-item').addClass("in-animated-first");
+        $('.carousel-element').eq(0).addClass("in-animated-first");
         setTimeout(function () {
             $('.in-animated-first').removeClass('in-animated-first');
-        }, 2000);
+        }, 720);
     }
 
     function generateItemLast() {
@@ -66,27 +50,26 @@ $(function () {
         var imagesrc = (randomValue <= 0.5) ? 'img/boy_1.png' : 'img/boy_2.png';
         $('<img>', { src: imagesrc}).appendTo('.empty-last');
         $('<div><div></div></div>').appendTo('.empty-last');
-        $('.last-item').removeClass('last-item')
-        $('.empty-last').removeClass('empty-last').addClass('last-item');
+        $('.empty-last').removeClass('empty-last');
         setTimeout(function () {
             $('<li>', {class: 'carousel-element empty-last'}).appendTo('.carousel-list');
-        }, 2000);
+            $('.empty-last').css({marginRight: -140+"px"}).animate({marginRight:25+'px'},700);
+        }, 720);
 
     }
 
     function moveItemAwayFirst() {
-        $('.first-item').addClass("away-animated-first");
+        $('.carousel-element').eq(1).addClass("away-animated-first");
         setTimeout(function () {
             $('.away-animated-first').remove();
-            $('.carousel-element').eq(1).addClass('first-item')
-        }, 2000);
+        }, 1000);
     }
 
     function moveItemInLast() {
-        $('.last-item').addClass("in-animated-last");
+        $('.carousel-element').eq(4).addClass("in-animated-last");
         setTimeout(function () {
             $('.in-animated-last').removeClass('in-animated-last');
-        }, 2000);
+        }, 720);
     }
 
 });
